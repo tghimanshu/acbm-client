cfbdd295a63b98a1b854872651fe232a416ea4b8
Himanshu Gohil <himanshugohil234@gmail.com>
2022-12-27 09:05:04 +0530

    added firebase config using local env

--- firebase.js
+++ firebase.js
@@ -1,10 +1,23 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-const config = {};
+// config
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID,
+};
 
-const app = initializeApp(config);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
+// exported all firebase parts
+export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
